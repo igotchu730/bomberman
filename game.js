273bc77a7cd7d8f7c8d52e64232d501508b8b3ae
igotchu730 <145312829+igotchu730@users.noreply.github.com>
2025-04-28 22:06:56 -0700

    Implement player movement
    Implemented player movement with animations. Setup controls and physics.

--- game.js
+++ game.js
@@ -1,16 +1,26 @@
 import Phaser from 'phaser';
 
 const config = {
+    /* How phaser renders the game */
     type: Phaser.AUTO,
     width: 544,
     height: 416,
     backgroundColor:'#1d1d1d',
+    /* Render game for pixels, preserves sharp blocky look */
     pixelArt: true,
+    /* Define physics engine of the game */
+    physics: {
+        default: 'arcade', // basic arcade engine
+        arcade: {
+            debug: true // shows outlines around bodies, for testing collisions
+        }
+    },
     scene: {
         preload,
         create,
         update
     },
+    /* Makes sure the game scales to fit screen*/
     scale: {
         mode: Phaser.Scale.FIT,
     }
@@ -35,7 +45,12 @@ function create(){
     /* Define all directions */
     const directions = ['front', 'right', 'left', 'back'];
 
-    /* Define all animation data */
+
+    /* 
+        ---PLAYER ANIMATIONS---
+    */
+   
+    /* Define all player animation data */
     const playerAnimations = {
         idle: { startFrames: { front: 0, right: 16, left: 32, back: 48 }, frameRate: 3 },
         walk: { startFrames: { front: 1, right: 17, left: 33, back: 49 }, frameRate: 7 },
@@ -43,10 +58,7 @@ function create(){
         death: { startFrames: { front: 3, right: 19, left: 35, back: 51 }, frameRate: 7 }
     };
 
-    /* 
-        -PLAYER ANIMATIONS-
-        Loop through all player animation data and create directional animations for each 
-    */
+    /* Loop through all player animation data and create directional animations for each */
     for(const [type, {startFrames, frameRate}] of Object.entries(playerAnimations)){
         directions.forEach(direction => {
             const start = startFrames[direction];
@@ -64,17 +76,82 @@ function create(){
         });
     }
 
-    /* Add player character */
-    this.player = this.add.sprite(272, 208,'character');
+    /* Add player character as a physics sprite */
+    this.player = this.physics.add.sprite(272, 208,'character');
+
+    /* Make the player look bigger, Make collision box smaller */
+    this.player.setScale(2);
+    this.player.body.setSize(16, 16).setOffset(8, 8); 
+
+
+    /* Player character collides with world bounds */
+    this.player.setCollideWorldBounds(true);
+
+    /* Create moving keys */
+    this.cursors = this.input.keyboard.createCursorKeys(); // arrow keys
+    this.wasd = this.input.keyboard.addKeys({ // WASD keys
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+
+    /* Keeps track of where the player is facing, defaults to front */
+    this.lastFacedDirection = 'idle-front';
 
     /* Play idle animation */
-    this.player.play('walk-front');
+    this.player.play(this.lastFacedDirection);
 
 };
 
 
 function update(){
 
+    const speed = 80; // player speed
+    const player = this.player; // player object
+    const cursors = this.cursors; // cursor object
+    const wasd = this.wasd; // wasd object
+
+    /* resets movement every frame so player only moves on key pressed */
+    player.setVelocity(0);
+
+    /* Check if multiple movement keys are pressed in an array, filter for true values */
+    const keysPressed = [
+        cursors.left.isDown || wasd.left.isDown,
+        cursors.right.isDown || wasd.right.isDown,
+        cursors.up.isDown || wasd.up.isDown,
+        cursors.down.isDown || wasd.down.isDown
+    ].filter(Boolean);
+    /* If multiple movement keys are pressed at once, stop moving */
+    if(keysPressed.length>1){
+        player.play(this.lastFacedDirection, true);
+        return;
+    }
+
+    /* Player movement by arrow and wasd keys, animated */
+    if(cursors.left.isDown || wasd.left.isDown){
+        player.setVelocityX(-speed);
+        player.anims.play('walk-left',true);
+        this.lastFacedDirection = 'idle-left';
+    }
+    else if (cursors.right.isDown || wasd.right.isDown){
+        player.setVelocityX(speed);
+        player.anims.play('walk-right',true);
+        this.lastFacedDirection = 'idle-right';
+    }
+    else if (cursors.up.isDown || wasd.up.isDown){
+        player.setVelocityY(-speed);
+        player.anims.play('walk-back',true);
+        this.lastFacedDirection = 'idle-back';
+    }
+    else if (cursors.down.isDown || wasd.down.isDown){
+        player.setVelocityY(speed);
+        player.anims.play('walk-front',true);
+        this.lastFacedDirection = 'idle-front';
+    }
+    else{
+        player.play(this.lastFacedDirection, true) // player faces last known direction
+    }
 };
 
 new Phaser.Game(config);
